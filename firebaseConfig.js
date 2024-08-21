f3ca53b7b6f5235ded004022bd88704db8240f65
zacHub-alt <lac.zuh@gmail.com>
2024-08-21 11:41:02 +0100

    Update

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,13 +6,13 @@ import { getAnalytics, isSupported } from 'firebase/analytics';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCIkQ2ll7--_6ZTyycyGpW-yApSFHWPAB8",
-  authDomain: "chatbot-1311e.firebaseapp.com",
-  projectId: "chatbot-1311e",
-  storageBucket: "chatbot-1311e.appspot.com",
-  messagingSenderId: "725798771564",
-  appId: "1:725798771564:web:d53ac25e9f0b8fecfe7149",
-  measurementId: "G-YQY8R6CRX9"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
